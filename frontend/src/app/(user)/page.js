0a09429ebd4e8dfcedcc56406d2c28c451babf79
Bibekl-lamichhane <bibek1999lamichhane@gmail.com>
2026-06-03 12:01:00 +0545

    localhost fix

--- frontend/src/app/(user)/page.js
+++ frontend/src/app/(user)/page.js
@@ -10,12 +10,12 @@ const page = () => {
 const [products,setProducts]=useState([])
 const [Loading,setLoading]=useState(true)
 
-const ref=useRef()
+
    useEffect(() => {
     const fetchProducts = async () => {
       try {
 
-         const response = await fetch("http://localhost:8000/api/products");
+         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
         if (!response.ok) {
         throw new Error("Failed to fetch");
         }
@@ -39,7 +39,7 @@ const ref=useRef()
           <CircularProgress size={40} />
         </div>
       ) : (
-        <div id="shopnow-section" ref={firstLoadRef} className="flex flex-col gap-2">
+        <div id="shopnow-section"  className="flex flex-col gap-2">
              <div  className="text-3xl text-orange-400 font-bold ml-8">Shop Now: </div>
              <div className='grid grid-cols-2 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
                {products.map((items, key) => (
